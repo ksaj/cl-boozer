8cd832d7f0efabcbf94c14a9a6200e9357639452
Karsten Johansson <KarstenJohansson@gmail.com>
2019-07-21 01:42:40 -0400

    (force-output) for non-clisp flavours

--- boozer.lisp
+++ boozer.lisp
@@ -1,14 +1,18 @@
 (defun booze ()
   (princ "Enter bottle size in mL: ")
+  (force-output)
   (setq  bottle-size (read))
 
   (princ "Enter price in dollars: ")
+  (force-output)
   (setq  bottle-price (read))
 
   (princ "Enter abv: ")
+  (force-output)
   (setq  abv (read))
 
   (princ "Enter bottle return in cents: ")
+  (force-output)
   (setq  bottle-return (/ (read) 100))
 
   (format t "That's $~3$ per mL of pure alcohol."
