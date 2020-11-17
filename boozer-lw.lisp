78d0c3b305f79492362ba122b085d48576426679
Karsten Johansson <KarstenJohansson@gmail.com>
2020-11-16 19:00:51 -0500

    Boozer for LispWorks
    Uses CAPI, so you'll need some non-cli graphics for this one.

--- boozer-lw.lisp
+++ boozer-lw.lisp
@@ -5,18 +5,18 @@ Boozer for LispWorks
 Compare alcohol prices between different beverages of different sizes by
 calculating the cost per mL of pure alcohol. 
 
-Please Drink Responsibly. And cheaper!
+Please Drink Responsibly. And cheaply!
 
 |#
 
 (defun lw-boozer ()
  "Asks a few questions, then tells you how much the alcohol content costs per mg."
-  (let ((bottle-size   (capi:prompt-for-number "Enter bottle size in mL:"))
-        (bottle-price  (capi:prompt-for-number "Enter price in dollars:"))
-        (abv           (capi:prompt-for-number "Enter ABV:"))
-        (bottle-return (capi:prompt-for-number "Enter bottle return in cents:")))
+  (let ((bottle-size    (capi:prompt-for-number "Enter bottle size in mL:"))
+        (bottle-price   (capi:prompt-for-number "Enter price in dollars:"))
+        (abv            (capi:prompt-for-number "Enter ABV:"))
+        (bottle-return  (capi:prompt-for-number "Enter bottle return in cents:")))
 
   (capi:display-message "That's $~3$ per mL of pure alcohol."
-    (/ (- bottle-price (/ bottle-return 100)) (* bottle-size (/ abv 100))))))
+    (/  (- bottle-price (/ bottle-return 100))  (* bottle-size (/ abv 100))))))
 
 (lw-boozer)
